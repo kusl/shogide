99bdf8bb63aa2900165e74e37afe16c1b12876d0
Kushal Hada <kushaldeveloper@gmail.com>
2018-02-24 19:17:13 -0500

    add connection string

--- web/public/okee/social/config/config.php
+++ web/public/okee/social/config/config.php
@@ -4,13 +4,9 @@ session_start();
 
 $timezone = date_default_timezone_set("Europe/London");
 
-$con = mysqli_connect("localhost", "root", "", "okee-social"); //Connection variable
-
-//$con = mysqli_connect("db721108910.db.1and1.com", "dbo721108910", "Okeeph0enix2#", "db721108910"); //Connection variable
+$con = mysqli_connect("localhost", "root", "root", "test"); 
 
 if(mysqli_connect_errno())
 {
 	echo "Failed to connect: " . mysqli_connect_errno();
 }
-
-?>
